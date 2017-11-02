0f9b4dcb44477fb4e477df8df31cdf6f57eaefea
Angelos Evripiotis <jevripiotis@bloomberg.net>
2017-11-02 13:35:52 +0000

    fixup! WIP: benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,18 @@
 """Measure the performance of BuildStream CLI from the user's perspective."""
+
 import argparse
+import collections
 import contextlib
+import json
 import os
 import pathlib
+import shutil
 import subprocess
 import sys
+import tarfile
 import tempfile
 import time
+import uuid
 
 
 def main():
@@ -16,18 +22,21 @@ def main():
         type=int,
         default=3,
         help='How many times to repeat each measurement')
+    parser.add_argument(
+        '--erase-buildstream-cache',
+        action='store_true')
     args = parser.parse_args()
 
-    run_benchmark(benchmark_python, args.samples)
-    run_benchmark(benchmark_help, args.samples)
-    # run_benchmark(benchmark_files, args.samples)
-
-
-def run_benchmark(benchmark_fn, num_samples):
-    timer = Timer()
-    for i in range(num_samples):
-        benchmark_fn(timer)
-    print(timer.elapsed / num_samples)
+    # run_benchmark(args, benchmark_python)
+    # run_benchmark(args, benchmark_help)
+    # run_benchmark_series(
+    #     args,
+    #     benchmark_import_local_files,
+    #     (1, 5, 10, 20, 50, 100, 120, 150, 200))
+    run_benchmark_series(
+        args,
+        benchmark_import_files_tar,
+        range(1, 200 * 1000, 10 * 1000))
 
 
 def benchmark_python(timer):
@@ -40,89 +49,173 @@ def benchmark_help(timer):
         run('bst', '--help')
 
 
-def benchmark_files(timer):
+def benchmark_import_local_files(timer, num_files):
+    with make_bst_project() as p:
+        write_random_files(p.working_dir, 'files', num_files)
+        p.write_file('import.bst', make_local_import_text('files/'))
+        p.write_file('compose.bst', make_single_compose_text('import.bst'))
+
+        with timer.context('show compose element'):
+            p.bst('show', 'compose.bst')
+        with timer.context('build import element'):
+            p.bst('build', 'import.bst')
+        with timer.context('build compose element'):
+            p.bst('build', 'compose.bst')
+
+
+def benchmark_import_files_tar(timer, num_files):
+    with make_bst_project() as p:
+        write_random_files(p.working_dir, 'files', num_files)
+        tar_gz_directory(
+            os.path.join(p.working_dir, 'files.tar.gz'),
+            os.path.join(p.working_dir, 'files/'))
+        shutil.rmtree(os.path.join(p.working_dir, 'files/'))
+
+        p.write_file('import.bst', make_tar_import_text('files.tar.gz'))
+        p.write_file('compose.bst', make_single_compose_text('import.bst'))
+
+        with timer.context('track import element'):
+            p.bst('track', 'import.bst')
+        with timer.context('show compose element'):
+            p.bst('show', 'compose.bst')
+        with timer.context('build import element'):
+            p.bst('build', 'import.bst')
+        with timer.context('build compose element'):
+            p.bst('build', 'compose.bst')
+
+
+def run_benchmark(args, benchmark_fn):
     timer = Timer()
+    for i in range(args.samples):
+        if args.erase_buildstream_cache:
+            erase_buildstream_cache()
+        benchmark_fn(timer)
 
-    for i in range(3):
-        with make_fixture(FileImportFixture) as f:
-            with timer.context():
-                result = f.bst('build', f.files_bst)
-                print(result.stdout)
-                print(result.stderr)
-            # f.bst('build', f.compose_bst)
+    for key in timer.ordered_names:
+        name = benchmark_fn.__name__[len('benchmark_'):]
+        if key is not None:
+            name += ': ' + key
+        print(
+            name,
+            '{:.2f}'.format(timer.elapsed[key] / args.samples),
+            sep=',')
 
-    print(timer.elapsed)
+
+def run_benchmark_series(args, benchmark_fn, series):
+    for item in series:
+        timer = Timer()
+        for i in range(args.samples):
+            if args.erase_buildstream_cache:
+                erase_buildstream_cache()
+            benchmark_fn(timer, item)
+
+        for key in timer.ordered_names:
+            name = benchmark_fn.__name__[len('benchmark_'):]
+            if key is not None:
+                name += ':' + key
+            print(
+                name,
+                item,
+                '{:.2f}'.format(timer.elapsed[key] / args.samples),
+                sep=',')
 
 
 class Timer:
 
     def __init__(self):
-        self.elapsed = 0
+        self.elapsed = collections.defaultdict(lambda: 0.0)
+        self.ordered_names = []
 
     @contextlib.contextmanager
-    def context(self):
+    def context(self, name=None):
+        if name not in self.elapsed:
+            self.ordered_names.append(name)
         t = time.perf_counter()
         yield
-        self.elapsed += time.perf_counter() - t
+        self.elapsed[name] += time.perf_counter() - t
 
 
 @contextlib.contextmanager
-def make_fixture(klass):
+def make_bst_project():
     with tempfile.TemporaryDirectory() as dirname:
-        yield klass(dirname)
+        yield BstProject(dirname)
 
 
-IMPORT_TEMPLATE = """
-kind: import
-
-sources:
-- kind: local
-  path: {path}
-""".strip()
-
-
-COMPOSE_TEMPLATE = """
-kind: compose
-
-depends:
-- filename: {depend}
-  type: build
-""".strip()
+def make_tar_import_text(url):
+    return json.dumps({
+        'kind': 'import',
+        'sources': [
+            {'kind': 'tar', 'url': url}
+        ]
+    })
 
 
-class FileImportFixture():
+def make_local_import_text(path):
+    return json.dumps({
+        'kind': 'import',
+        'sources': [
+            {'kind': 'local', 'path': path}
+        ]
+    })
 
-    def __init__(self, working_directory):
-        self.working_directory = working_directory
-        self.write_file('project.conf', 'name: benchmark')
-        self.write_file('files/a', 'hello')
 
-        self.files_bst = 'files.bst'
-        self.write_file(
-            self.files_bst,
-            IMPORT_TEMPLATE.format(path='files'))
+def make_single_compose_text(depend_path):
+    return json.dumps({
+        'kind': 'compose',
+        'depends': [
+            {'filename': depend_path, 'type': 'build'}
+        ]
+    })
 
-        compose_bst = 'compose.bst'
-        self.write_file(
-            compose_bst,
-            COMPOSE_TEMPLATE.format(depend=self.files_bst))
+
+class BstProject():
+
+    def __init__(self, working_dir):
+        self.working_dir = working_dir
+        self.write_file('project.conf', 'name: ' + random_name())
 
     def write_file(self, relative_path, contents):
-        path = os.path.join(self.working_directory, relative_path)
+        path = os.path.join(self.working_dir, relative_path)
         write_file(path, contents)
 
     def bst(self, *args):
-        return run('bst', *args, working_dir=working_directory)
+        return run('bst', *args, working_dir=self.working_dir)
+
+
+def random_name():
+    return uuid.uuid4().hex
+
+
+def write_random_files(root, dirname, num_files):
+    for path in yield_random_paths(num_files):
+        write_file(os.path.join(root, dirname, path), random_name())
+
+
+def yield_random_paths(count):
+    for i in range(count):
+        name = uuid.uuid4().hex
+        yield os.path.join(name[:2], name[2:4], name[4:])
 
 
 def run(command, *args, working_dir=None):
-    return subprocess.run(
-        (command,) + args,
-        cwd=working_dir,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-        universal_newlines=True)
+    try:
+        return subprocess.run(
+            (command,) + args,
+            cwd=working_dir,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+            universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        print(e.stdout)
+        print(e.stderr)
+        raise
+
+
+def tar_gz_directory(tgz_path, dir_path):
+    with tarfile.open(tgz_path, "w:gz") as tar:
+        tar.add(dir_path, arcname=os.path.basename(dir_path))
 
 
 def write_file(path, contents):
@@ -132,6 +225,11 @@ def write_file(path, contents):
         parent.mkdir(parents=True)
     with open(path, "w") as f:
         f.write(contents)
+
+
+def erase_buildstream_cache():
+    cache_path = os.path.expanduser('~/.cache/buildstream')
+    shutil.rmtree(cache_path)
 
 
 if __name__ == '__main__':
