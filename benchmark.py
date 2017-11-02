5bcb8c608abae9d3dc52d907a0e0a5e75be85df5
Angelos Evripiotis <jevripiotis@bloomberg.net>
2017-11-02 16:10:44 +0000

    fixup! WIP: benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+#
+#  Copyright Bloomberg Finance LP
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors:
+#        Angelos Evripiotis <jevripiotis@bloomberg.net>
+
 """Measure the performance of BuildStream CLI from the user's perspective."""
 
 import argparse
